2879377e99e714ee47bb9f56b5ea3f03c68d7efe
Line Moseng <line.moseng@nav.no>
2018-06-05 12:54:07 +0200

    Added some very simple logging

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,32 @@
 const express = require('express')
+const logger = require('winston')
 const app = express()
 const port = 5000
 
 app.get('/', (request, response) => {
-  response.send('Hello from Express!')
+	response.send('Hello from Express!')
+	logger.info('Responded to GET request on /')
 })
 
 app.get('/api/me', (request, response) => {
-  response.send('{"name": "Arthur Dent", "location": "Earth"}')
+	response.send('{"name": "Arthur Dent", "location": "Earth"}')
+	logger.info('Responded to GET request on /api/me')
 })
 
 app.get('/api/work', (request, response) => {
-  response.send('[{"yearFrom": "2010", "yearTo": "2018", "place": "BBC Radio", "comment": "Was doing stuff"}, {"yearFrom": "2009", "yearTo": "2010", "place": "The coffee shop", "comment": "Making coffee"}]')
+	response.send('[{"yearFrom": "2010", "yearTo": "2018", "place": "BBC Radio", "comment": "Was doing stuff"}, {"yearFrom": "2009", "yearTo": "2010", "place": "The coffee shop", "comment": "Making coffee"}]')
+	logger.info('Responded to GET request on /api/work')
 })
 
 app.get('/api/education', (request, response) => {
-  response.send('[{"yearFrom": "2005", "yearTo": "2007", "place": "University 1", "comment": "Studying"}, {"yearFrom": "2001", "yearTo": "2004", "place": "Barista academy", "comment": "Made coffee"}]')
+	response.send('[{"yearFrom": "2005", "yearTo": "2007", "place": "University 1", "comment": "Studying"}, {"yearFrom": "2001", "yearTo": "2004", "place": "Barista academy", "comment": "Made coffee"}]')
+	logger.info('Responded to GET request on /api/edu')
 })
 
 app.listen(port, (err) => {
   if (err) {
-    return console.log('something bad happened', err)
+    return logger.error('something bad happened', err)
   }
 
-  console.log(`server is listening on ${port}`)
+  logger.info(`server is listening on ${port}`)
 })
