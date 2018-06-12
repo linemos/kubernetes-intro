940ef8a5940bbb85b613227a71addc0272b7c862
Ingrid <ingrid@vibbio.com>
2018-06-12 20:07:22 +0200

    Adding new text

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/me', (request, response) => {
-	response.send('{"name": "Arthur Dent", "location": "Earth"}');
+	response.send('{"name": "Ingrid Guren", "location": "Oschlo"}');
 	logger.info('Responded to GET request on /api/me')
 });
 
